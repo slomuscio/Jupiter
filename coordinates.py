dc0f4f7978aab1eea97a4093c389ccc568300fe5
slomuscio <52711910+slomuscio@users.noreply.github.com>
2019-07-19 11:39:39 -0400

    Add files via upload

--- coordinates.py
+++ coordinates.py
@@ -6,6 +6,7 @@ Time steps must be entered as floats in units of days
 Returns SkyCoord objects containing coordinates, either equitorial or galactic, of Jupiter at the given time or over the given timerange.
 
 Samantha Lomuscio
+Updated: 2019-07-17
 """
 
 from astropy.coordinates import solar_system_ephemeris, get_body, SkyCoord
@@ -18,20 +19,20 @@ def equatorial(obj=None, tstart=None, tend=None, tstep=None, mode=None, time=Non
 
     if time is not None and (tstart is not None or tend is not None or tstep is not None):
         raise AssertionError('Must enter one time, or a start time, end time, and time step')
-
     elif time is not None:
         with solar_system_ephemeris.set('jpl'):
             jup_ephem= get_body(obj, time)
-
     elif time is None and (tstart is None or tend is None or tstep is None):
         raise AssertionError('Must enter start time, end time, and time step')
-
     elif time is None and (tstart is not None and tend is not None and tstep is not None):
+
         if mode == 'c':
             tstart = tstart.mjd + (tstep/2) #use this line if you want times in the center of timeranges, dont use if you want coordinates at the time intervals themselves
+            tend = tend.mjd + (tstep/2)
         elif mode == 'b':
             tstart = tstart.mjd # use if you want coordinates at time intervals not at center of intervals
-        tend = tend.mjd
+            tend = tend.mjd
+
         times = np.arange(tstart, tend, tstep, dtype=float)
         times_list = Time(times, format='mjd').fits
         times_list = Time(times_list, format='fits')
@@ -62,10 +63,12 @@ def galactic(obj=None, tstart=None, tend=None, tstep=None, time=None, mode=None)
     elif time is None and (tstart is not None and tend is not None and tstep is not None):
         if mode == 'c':
             tstart = tstart.mjd + (tstep/2)  #use this line if you want times in the center of timeranges, dont use if you want coordinates at the time intervals themselves 
+	    tend = tend.mjd + (tstep/2)
         elif mode == 'b':
             tstart= tstart.mjd # use if you want coordinates at time intervals not at center of intervals
-        tend = tend.mjd
-        times = np.arange(tstart, tend, tstep, dtype=float)
+            tend = tend.mjd
+       
+	times = np.arange(tstart, tend, tstep, dtype=float)
         times_list = Time(times, format='mjd').fits
         times_list = Time(times_list, format='fits')
 
